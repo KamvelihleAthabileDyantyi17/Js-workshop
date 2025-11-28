7e6260024c943dece99d6ff9c3741475a424611c
Kamv'elihle Dyantyi <162297812+KamvelihleAthabileDyantyi17@users.noreply.github.com>
2025-11-28 19:06:30 +0200

    Initial commit: Added all JavaScript project folders

---
+++ BasicsOfJS/index.js
@@ -0,0 +1,65 @@
+//console.log('Hello');
+//console.log('I love Manchester United')
+
+/*This is to get that message alert, 
+you must first answere the window alert before the website displays
+window.alert('This is an alert')
+window.alert('This is an alert , Emergency')
+*/
+/*This is another way to display text
+this done by creating a id i used the h1 and p ids i dont
+know what will happen if theirs is information in both the js and html
+document.getElementById("Myh1").textContent= 'hELLO';
+document.getElementById("Myh1").style.color = 'red';
+document.getElementById("MyP").textContent = "I like playing Ps4";
+*/
+//variable = A container that stores a value.
+//           behaves as if it were the value it contains
+
+
+//-------------------------------------------
+//Variables
+// declaration   let x;
+// initialization  x = 5;
+
+let age = 24;
+let price = 17.98;
+let average = 25;
+
+// to display the data type of a variable 
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof average);
+// this is to display a variable
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price} dollars`);
+console.log(`The average is ${average} years`);
+
+//-------------------------------------------
+//String
+let firstName = "Kamv'elihle"
+let lastName = "Dyanti"
+
+// to display a string
+console.log(typeof firstName);
+console.log(typeof lastName);
+
+// to display a string with a variable
+console.log(`My name is ${firstName} ${lastName}`);
+
+//--------------------------------------------
+//booleans
+let online = true;
+
+console.log(`Kamva is online: ${online}`);
+
+
+
+
+
+
+
+
+
+
+
