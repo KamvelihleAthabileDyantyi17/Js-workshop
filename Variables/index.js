a9bbe2351937786da233c445c0c8cd06601fcf5f
Kamv'elihle Dyantyi <162297812+KamvelihleAthabileDyantyi17@users.noreply.github.com>
2025-11-28 19:12:37 +0200

    Added the Variables project folder

---
+++ Variables/index.js
@@ -0,0 +1,7 @@
+// Let can be changed but if i use the variable const it cannot be changed
+// Used cd to chnage the directory  variables and using node index.js to run it inside the variables folder 
+let pi = 3.14; 
+
+pi = 10; // This reassignment is now valid
+
+console.log(pi); // Output will be: 10
