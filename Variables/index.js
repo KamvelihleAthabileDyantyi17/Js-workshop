87abba263ac8d7c83c97cb1d6275ff2bbd6264c5
Kamv'elihle Dyantyi <162297812+KamvelihleAthabileDyantyi17@users.noreply.github.com>
2025-11-28 19:30:04 +0200

    Example 3 logic

--- Variables/index.js
+++ Variables/index.js
@@ -1,7 +1,16 @@
 // Let can be changed but if i use the variable const it cannot be changed
 // Used cd to chnage the directory  variables and using node index.js to run it inside the variables folder 
+/*
 let pi = 3.14; 
 
 pi = 10; // This reassignment is now valid
 
 console.log(pi); // Output will be: 10
+*/
+
+let number1 = 34; // number1 is declared and initialized with 34
+let number2 = 78;
+
+
+
+console.log(number1 + number2);
